a1749ea2394a466e3a1391104c908fddbc7fd0ee
Danilo Ribeiro <danilo.silva@yandeh.com.br>
2024-05-23 22:23:07 -0300

    fix env

--- .devops/terraform/07-nodes.tf
+++ .devops/terraform/07-nodes.tf
@@ -43,9 +43,9 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 0
+    desired_size = 1
     max_size     = 5
-    min_size     = 0
+    min_size     = 1
   }
 
   update_config {
